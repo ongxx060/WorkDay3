e792ae6bec1b4811cbe4ed0ed39133274c18271e
idsc3101 <idsc3101@gmail.com>
2013-09-23 15:23:44 -0500

    commit

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -6,19 +6,39 @@ Public Class Form1
 
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
         Dim num1, num2, largeNum As Double
-        num1 = CDbl(txtFirstNum.Text)
-        num1 = CDbl(txtSecondNum.Text)
-        If (num1 > num2) Then
-            txtResult.Text = "the larger number is" & num1
-            largerNum = num1
-        ElseIf (num1 < num2) Then
-            largeNum = num2
-            txtResult.Text = "The larger number is" & num2
+
+        If IsNumeric(txtFirstNum.Text) Or IsNumeric(txtSecondNum.Text) Then
+            num1 = CDbl(txtFirstNum.Text)
+            num2 = CDbl(txtSecondNum.Text)
+
+            If (num1 > num2) Then
+                txtResult.Text = "Larger number is" & num1
+            ElseIf (num2 < num1) Then
+
+                txtResult.Text = "Larger number is" & num2
+
+            Else
+                txtResult.Text = "the two are equal"
+
+            End If
 
         Else
-            txtResult.Text = "they are equal"
 
+            MessageBox.Show("Error: Please type a number")
+            Return
         End If
+        num1 = CDbl(txtFirstNum.Text)
+        num2 = CDbl(txtSecondNum.Text)
+        If (num1 > num2) Then
+            textresult.text = "larger number is" & num1
+        ElseIf (num2 > num1) Then
+            txtResult.Text = "larger number is" & num2
+        Else
+            txtResult.Text = "the two are equal"
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
 
     End Sub
 End Class
